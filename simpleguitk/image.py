fd343d5c2f381e7b0a930aa2cfca40729cc9dd6e
David Holm <dholmster@gmail.com>
2013-05-18 23:21:17 +0200

    Support image versions

--- simpleguitk/image.py
+++ simpleguitk/image.py
@@ -9,7 +9,7 @@ class Image(object):
         image = urllib.urlretrieve(url)[0]
         self._image = PilImage.open(image)
         self._image = self._image.convert('RGBA')
-        self._tkimage = None
+        self._versions = {}
 
     def get_width(self):
         return self._image.size[0]
@@ -18,14 +18,15 @@ class Image(object):
         return self._image.size[1]
 
     def _get_tkimage(self, center, wh_src, wh_dst, rot):
-        if self._tkimage is None:
+        version = ','.join([str(center), str(wh_src), str(wh_dst), str(rot)])
+        if version not in self._versions:
             crop = (center[0] - wh_src[0] / 2, center[1] - wh_src[1] / 2,
                     center[0] + wh_src[0] / 2, center[1] + wh_src[1] / 2)
             image = self._image.crop(crop)
             image = image.resize(wh_dst, resample=PilImage.BILINEAR)
             image = image.rotate(-rot, resample=PilImage.BICUBIC, expand=1)
-            self._tkimage = ImageTk.PhotoImage(image)
-        return self._tkimage
+            self._versions[version] = ImageTk.PhotoImage(image)
+        return self._versions[version]
 
 
 def load_image(URL):
