886234f91d1cfca5435df974e204d93d43432655
David Holm <dholmster@gmail.com>
2013-05-25 18:22:45 +0200

    Fixes some minor bugs in sound API

--- simpleguitk/sound.py
+++ simpleguitk/sound.py
@@ -2,24 +2,32 @@ import urllib
 
 
 class Sound(object):
-    def __init__(self, url):
+    def __init__(self, url, channel):
         import pygame
+        self._channel = pygame.mixer.Channel(channel)
         soundfile = urllib.urlretrieve(url)[0]
         self._sound = pygame.mixer.Sound(soundfile)
+        self._paused = False
 
     def play(self):
-        self._sound.play()
+        if self._paused:
+            self._paused = False
+            self._channel.unpause()
+        else:
+            self._channel.play(self._sound)
 
     def pause(self):
-        self._sound.pause()
+        self._channel.pause()
+        self._paused = True
 
     def rewind(self):
-        self._sound.rewind()
+        self._channel.stop()
 
     def set_volume(self, volume):
-        self._sound.set_volume(volume)
+        self._channel.set_volume(volume)
 
 _initialized = False
+_next_channel = 0
 
 
 def sound_init():
@@ -30,7 +38,9 @@ def sound_init():
 
 
 def load_sound(URL):
+    global _next_channel
     if not _initialized:
         sound_init()
 
-    return Sound(URL)
+    _next_channel += 1
+    return Sound(URL, _next_channel - 1)
