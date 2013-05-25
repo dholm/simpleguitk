73738fe1a45894eccd1f029a65be44cac97ffb15
David Holm <dholmster@gmail.com>
2013-05-25 22:07:16 +0200

    Translates several keys to JavaScript keycodes to be compatible with raw keycodes

--- simpleguitk/input.py
+++ simpleguitk/input.py
@@ -2,9 +2,26 @@
 # This file is part of SimpleGUITk - https://github.com/dholm/simpleguitk
 # See the file 'COPYING' for copying permission.
 
+
 class KeyMap(object):
+    def __init__(self):
+        # JavaScript keycode translations
+        self._tr = {'up': 38, 'down': 40, 'left': 37, 'right': 39,
+                    'space': 32, 'return': 13, 'tab': 9, 'backspace': 8,
+                    'comma': 188, 'minus': 189, 'period': 190, 'slash': 191}
+        nums = dict(zip([chr(x) for x in range(ord('0'), ord('9') + 1)],
+                        range(48, 58)))
+        self._tr.update(nums)
+        alphas = dict(zip([chr(x) for x in range(ord('a'), ord('z') + 1)],
+                          range(65, 91)))
+        self._tr.update(alphas)
+        fxs = dict(zip(['f%d' % x for x in range(1, 13)], range(112, 124)))
+        self._tr.update(fxs)
+
     def __getitem__(self, name):
-        return name.lower()
+        # Attempt to map key to keycode, otherwise just return the name so it
+        # can be matched by KEY_MAP[name].
+        return self._tr.get(name.lower(), name.lower())
 
 
 KEY_MAP = KeyMap()
@@ -24,11 +41,11 @@ class InputAdapter(object):
 
     def _keydown(self, key):
         if self._keydown_handler is not None:
-            self._keydown_handler(key.keysym.lower())
+            self._keydown_handler(KEY_MAP[key.keysym])
 
     def _keyup(self, key):
         if self._keyup_handler is not None:
-            self._keyup_handler(key.keysym.lower())
+            self._keyup_handler(KEY_MAP[key.keysym])
 
     def _mouse_click(self, event):
         if self._mouse_click_handler is not None:
