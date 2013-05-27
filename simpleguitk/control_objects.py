800d31e9df6af1ce5134901ef79a9a6e4090ab1f
David Holm <dholmster@gmail.com>
2013-05-27 19:45:16 +0200

    Attempts to better match SimpleGUI control object widths

--- simpleguitk/control_objects.py
+++ simpleguitk/control_objects.py
@@ -6,37 +6,36 @@ import Tkinter
 import tkFont
 
 
-class Widget(object):
-    def _config_width(self, text):
-        if self._width is not None and len(text):
-            font = tkFont.Font(font=self._widget.cget('font'))
-            avg_char_width = font.measure(text) / len(text)
-            width = self._width / avg_char_width
-            self._widget.config(width=width)
+_BaseString = ('This is a really boring basic string used for width '
+               'calculations.')
 
+
+class Widget(object):
     def __init__(self, widget, width):
         self._widget = widget
-        self._width = width
-        self._widget.grid(column=0)
+        if width is not None:
+            font = tkFont.Font(font=widget.cget('font'))
+            avg_char_width = font.measure(_BaseString) / len(_BaseString)
+            self._widget.config(width=width / avg_char_width)
+
+        self._widget.grid(column=0, sticky=Tkinter.W)
 
 
 class Button(Widget):
     def __init__(self, master, text, button_handler, width=None):
         button = Tkinter.Button(master, text=text, command=button_handler)
         super(Button, self).__init__(button, width)
-        self._config_width(text)
+        self.set_text(text)
 
     def set_text(self, text):
         self._widget.config(text=text)
-        self._config_width(text)
 
 
 class Label(Widget):
     def __init__(self, master, text):
         label = Tkinter.Label(master, text=text)
         super(Label, self).__init__(label, None)
-        text_width = max(len(text), 15)
-        self._widget.config(width=text_width)
+        self._widget.config(wraplength=350, justify=Tkinter.LEFT)
 
     def set_text(self, text):
         self._widget.config(text=text)
@@ -48,7 +47,6 @@ class Input(Widget):
         self._content = Tkinter.StringVar()
         entry = Tkinter.Entry(master, textvariable=self._content)
         super(Input, self).__init__(entry, width)
-        self._config_width(text)
 
         self._input_handler_fn = input_handler
         self._widget.bind('<Return>', self._input_handler)
